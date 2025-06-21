1055f6694bd549e8a6ae6d5ef4aa87cfd1c12634
Subed <subedshahxi@gmail.com>
2025-06-22 00:27:53 +0545

    feat: added feat to todo

--- src/components/todo/todoList.jsx
+++ src/components/todo/todoList.jsx
@@ -1,16 +1,69 @@
-import TaskForm from "./TaskForm";
+// import TaskForm from "./TaskForm";
 
-function TodoList() {
+// function TodoList() {
+//   return (
+//     <div className="grid grid-rows-[150px_auto]">
+//       <TaskForm />
+//       <main className="flex justify-evenly pt-[20px] pb-[20px] px-[8%]">
+//         <section className="w-1/3 bg-amber-300 m-[20px]">Section 1</section>
+//         <section className="w-1/3 bg-amber-300 m-[20px]">Section 2</section>
+//         <section className="w-1/3 bg-amber-300 m-[20px]">Section 3</section>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default TodoList;
+import React, { useState, useEffect } from "react";
+
+import TaskForm from "./components/TaskForm";
+import TaskColumn from "./components/TaskColumn";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
+
+const oldTasks = localStorage.getItem("tasks");
+
+const App = () => {
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleDelete = (taskIndex) => {
+    const newTasks = tasks.filter((task, index) => index !== taskIndex);
+    setTasks(newTasks);
+  };
+
   return (
-    <div className="grid grid-rows-[150px_auto]">
-      <TaskForm />
-      <main className="flex justify-evenly pt-[20px] pb-[20px] px-[8%]">
-        <section className="w-1/3 bg-amber-300 m-[20px]">Section 1</section>
-        <section className="w-1/3 bg-amber-300 m-[20px]">Section 2</section>
-        <section className="w-1/3 bg-amber-300 m-[20px]">Section 3</section>
+    <div className="app">
+      <TaskForm setTasks={setTasks} />
+      <main className="app_main">
+        <TaskColumn
+          title="To do"
+          icon={todoIcon}
+          tasks={tasks}
+          status="todo"
+          handleDelete={handleDelete}
+        />
+        <TaskColumn
+          title="Doing"
+          icon={doingIcon}
+          tasks={tasks}
+          status="doing"
+          handleDelete={handleDelete}
+        />
+        <TaskColumn
+          title="Done"
+          icon={doneIcon}
+          tasks={tasks}
+          status="done"
+          handleDelete={handleDelete}
+        />
       </main>
     </div>
   );
-}
+};
 
-export default TodoList;
+export default App;

144ec0bdc99e2d42344878e87677957f2a98d8ab
Subed <subedshahxi@gmail.com>
2025-06-25 15:19:55 +0545

    debug: header grid issue

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,20 +4,24 @@ import Header from "./Header";
 
 function AppLayout() {
   return (
-    <>
-      <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_1fr] h-screen ">
-        <Header />
+    <div className="grid h-screen grid-cols-[auto_1fr] grid-rows-[auto_1fr] grid-areas-layout">
+      {/* Sidebar in first column, spans all rows */}
+      <div className="row-span-2">
         <Sidebar />
-        <main
-          className="bg-grey-50 pt-16  pb-24 pl-[4.6rem]
-"
-        >
-          <div className="max-w-[120rem] mx-auto flex flex-col gap-8">
-            <Outlet />
-          </div>
-        </main>
       </div>
-    </>
+
+      {/* Header in top-right */}
+      <div className="col-start-2 row-start-1 z-10">
+        <Header />
+      </div>
+
+      {/* Main content below header, right of sidebar */}
+      <main className="col-start-2 row-start-2 bg-gray-50 p-6 overflow-y-auto">
+        <div className="max-w-[120rem] mx-auto flex flex-col gap-8">
+          <Outlet />
+        </div>
+      </main>
+    </div>
   );
 }
 

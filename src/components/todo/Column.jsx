eef99c5567d3796712d89d5cecfb6506e201d716
Subed <subedshahxi@gmail.com>
2025-06-25 14:14:34 +0545

    feat:added column

---
+++ src/components/todo/Column.jsx
@@ -0,0 +1,30 @@
+import { useDroppable } from "@dnd-kit/core";
+import Task from "./Task";
+
+export default function Column({ column, onDeleteTask }) {
+  const { setNodeRef, isOver } = useDroppable({ id: column.id });
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={{
+        backgroundColor: isOver ? "#555" : "#333",
+        padding: 16,
+        borderRadius: 6,
+        width: 220,
+        minHeight: 120,
+        marginRight: 16,
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <h3>{column.title}</h3>
+      <div style={{ flexGrow: 1, marginTop: 8 }}>
+        {column.tasks.map((task) => (
+          <Task key={task.id} task={task} onDelete={onDeleteTask} />
+        ))}
+      </div>
+    </div>
+  );
+}

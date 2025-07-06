e61bdc10e3ee26d5c7636712a0a44e9499a79019
Subed <subedshahxi@gmail.com>
2025-07-06 15:06:55 +0545

    added header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,7 +1,27 @@
+// import { IoSearchCircleOutline } from "react-icons/io5";
+import { BiSearch } from "react-icons/bi";
+import { LuBell } from "react-icons/lu";
+
 function Header() {
   return (
     <header className="bg-grey-0 px-[4.8rem] py-[1.2rem] border-b border-[var(--color-grey-100)]">
-      just a header
+      <div className="flex gap-5 justify-between items-center">
+        <div className=" flex w-5/6 bg-amber-400 rounded-xl justify-between p-2 items-center">
+          <div className="">search bar</div>
+          <div className="">
+            <BiSearch />
+          </div>
+        </div>
+        <div
+          className="flex
+        gap-3"
+        >
+          <div className="border-b-2 rounded-2xl">
+            <LuBell />
+          </div>
+          <div>user profile</div>
+        </div>
+      </div>
     </header>
   );
 }
